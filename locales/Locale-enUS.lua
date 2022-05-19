70fd33bcc7354e1df7f1898ecbf523e92452338b
Your Name <you@example.com>
2022-05-19 20:01:29 +0800

    add enUS locale

--- locales/Locale-enUS.lua
+++ locales/Locale-enUS.lua
@@ -1,2 +1,22 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("BGQueuer", "enUS", true)
 if not L then return end
+L["Play sound when a leader initiated a ready check"] 	= true
+L["Play sound even in mute mode"] 						= true
+L["Play sound when a leader initiated a role check"] 	= true
+L["Play sound when you are eligible to enter battle"] 	= true
+L["enable"]												= true
+L["Major option"]										= true
+L["Play sound when battle countdown almost ends"]		= true
+L["About 5 seconds before"]								= true
+L["Auto leave BG/Arena if ended"]						= true
+L["Leave and play sound"]								= true
+L["delay time"]											= true
+L["delay time to leave Battleground/Arena"]				= true
+L["Auto Release when player was died"]					= true
+L["About"]												= true
+L["Other"]												= true
+L["Play sound"]											= true
+L["Your battlefield group has been ready"]				= true
+L["Author"]												= true
+L["Version"]											= true
+L["An addon that help you queue for battlefield by play sound and \nnot to miss the eligibility to join/leave battlefield battle anymore."] = true
